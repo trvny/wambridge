2979c6807499d107813d8b8441e0daf08fb86c01
trvny <120686325+trvny@users.noreply.github.com>
2026-07-31 21:15:49 +0200

    build: package DMS diagnostic probe

--- foobar/wambridge_dlna_entry.py
+++ foobar/wambridge_dlna_entry.py
@@ -1,5 +1,5 @@
-"""Standalone DLNA playback probe for Samsung WAM speakers."""
+"""Standalone DMS playback diagnostic for Samsung WAM speakers."""
 
-from wambridge.dlna_cli import main
+from wambridge.dms_cli import main
 
 raise SystemExit(main())
